36c1b5b4b54a22e590346a62c767b4a4c9764343
sniperadmin <nasrbue@gmail.com>
2023-02-12 03:46:52 +0200

    1 binaries uploaded

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
-<script lang="ts"></script>
+<script lang="ts">
+// import { useDisplay } from 'vuetify';
+// const { mobile } = useDisplay();
+</script>
 
 <template>
-  <!-- <VueParticles /> -->
-  <!-- <NTimer /> -->
   <v-row justify="center">
-    <v-col cols="12" md="7" lg="6" class="d-flex align-center">
+    <v-col cols="6" class="d-flex align-center">
       <div class="text-center text-md-left">
         <v-chip small class="bg-dark" text-color="white">
           Creating Brands
         </v-chip>
         <h2 class="font-weight-bold text-uppercase white--text">
-          ONE BILLON PEOPLE USE FACEBOOK
+          I use science to bring leisure to startups
         </h2>
         <p class="white--text op-8 font-weight-regular">
           Pellentesque vehicula eros a dui pretium ornare. Phasellus congue vel
@@ -18,25 +19,25 @@
           nonn accumsan.
         </p>
         <div class="mt-16 pt-2">
-          <v-btn
+          <NBtn
             nuxt
             large
             rounded
+            label="Do you Need Help ?"
             class="btn-custom-md"
             depressed
             color="white"
             elevation="0"
-          >
-            Do you Need Help ?
-          </v-btn>
+          />
         </div>
       </div>
     </v-col>
-    <v-col cols="12" md="5" lg="5">
-      <!-- <v-img
-        :src="require('@/assets/images/banner/banner1.jpg')"
+    <v-col cols="6">
+      <v-img
+        src="/3d-business-man-standing-with-laptop-and-coffee.png"
         alt="banner"
-      /> -->
+        width="250"
+      />
     </v-col>
   </v-row>
 </template>
